dd72e11a2dbe1289a732e5c2f07ff254545ffb8d
Calem Bendell <calem.j.bendell@gmail.com>
2017-12-22 10:41:15 -0500

    basic import with example assets working

---
+++ Scripts/runimport.bat
@@ -0,0 +1,1 @@
+UE4Editor-Cmd.exe "C:\Users\calem\Documents\Unreal Projects\UnrealImportAssetsExamples\AssetImporting.uproject" -run=ImportAssets -importsettings="C:\Users\calem\Documents\Unreal Projects\UnrealImportAssetsExamples\Scripts\ExampleAssetsImportSettings.json" -nosourcecontrol
